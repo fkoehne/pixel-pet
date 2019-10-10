d48a6662bf6a31c03542cca37eb22306ef128af0
Frank Köhne <frank.koehne@viadee.de>
2019-10-10 21:41:33 +0200

    Create image display endpoint

--- code/petServer.py
+++ code/petServer.py
@@ -5,6 +5,13 @@ sense = SenseHat()
 
 app = Flask(__name__)
 
+sense.clear(0, 0, 0)
+neutral = sense.load_image("img/neutral.png")
+blinking = sense.load_image("img/blinking.png")
+eyesLeft = sense.load_image("img/EyesLeft.png")
+eyesRight = sense.load_image("img/EyesRight.png")
+bright = sense.load_image("img/8x8face.png")
+
 @app.route('/')
 def index():
     return "Hello, World!"
@@ -12,6 +19,13 @@ def index():
 @app.route('/api/temperature', methods=['GET'])
 def get_temperature():
     return str(sense.get_temperature())
+
+@app.route('/api/displayimage/<img>', methods=['GET'])
+def displayimage(img):
+    if img < 0:
+        abort(404)
+    sense.set_pixels(neutral) # TODO get faces from a map
+    return 'done' 
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
