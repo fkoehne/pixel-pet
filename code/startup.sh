b00afa8519a3334092ba6cd51e310c5d4c63aeb8
Frank Köhne <frank.koehne@viadee.de>
2019-10-29 21:46:23 +0100

    Run as user pi on startup

--- code/startup.sh
+++ code/startup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # launcher.sh
 # Run the pixel pet robot (ideally on startup)
-cd /home/pi/code/pixel-pet/code/
-sudo pi
-python api.py
+cd /home/pi/code/pixel-pet/code/ 
+su pi -c '/usr/bin/python gamepad.py'
 cd /
